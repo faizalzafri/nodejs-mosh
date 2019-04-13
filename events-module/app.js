c447c6db810d9f2988d7fc823536b773d2c5d1d0
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-14 02:30:33 +0530

    event arg added

--- events-module/app.js
+++ events-module/app.js
@@ -5,12 +5,12 @@ emitter.on('eventDemo', function () {
     console.log('Listener Added 1');
 });
 
-var eventListener = function () {
-    console.log('Listener Added 2');
+var eventListener = function (args) {
+    console.log('Listener Added 2', args);
 };
 
 emitter.on('eventDemo', eventListener);
 
 emitter.on('eventDemo', () => console.log('Listener Added 3'));
 
-emitter.emit('eventDemo');
+emitter.emit('eventDemo',{ id:1, name: 'Faizal'});
