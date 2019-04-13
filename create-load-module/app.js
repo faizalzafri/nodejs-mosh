ef8d30f2badd1c8fe64e7ca69c4a31ba89fd0e16
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-13 21:05:12 +0530

    module wrapper function

--- create-load-module/app.js
+++ create-load-module/app.js
@@ -1,7 +1,11 @@
+
 const logger = require('./logger');
 
 console.log(logger);
 
 //logger.log('Hi Faizal'); //remove comments from logger.js
 
-logger('Hi Faizal')
+logger('Hi Faizal');
+
+console.log(__filename);
+console.log(__dirname);
--- create-load-module/logger.js
+++ create-load-module/logger.js
@@ -1,10 +1,14 @@
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+
 var url = 'http://somelogger.io/log';
 
-function log(message){
-    //send a http request
-    console.log(message);
-}
+    function log(message) {
+        //send a http request
+        console.log(message);
+    }
 
-//module.exports.log = log;
+    //module.exports.log = log;
 
-module.exports = log;
+    module.exports = log;
