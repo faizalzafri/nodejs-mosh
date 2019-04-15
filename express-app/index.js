324e2bbba3584aea34c2140133b6180c9a9a2e34
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-15 22:03:04 +0530

    env vars and nodemon global
    npm i -g nodemon

--- express-app/index.js
+++ express-app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',(req,res)=>{
     res.send('Hi Faizal');
 });
 
-app.listen(3000, ()=>{
-    console.log('Listening..');
+app.listen(port, ()=>{
+    console.log(`Listening on..${port}`);
 });
