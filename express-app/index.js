6b6cdb7fde28bbf1c4774bee3392ddc4a3852fc2
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-15 22:10:18 +0530

    route parameters

--- express-app/index.js
+++ express-app/index.js
@@ -7,6 +7,17 @@ app.get('/',(req,res)=>{
     res.send('Hi Faizal');
 });
 
+app.get('/api/courses/:id', (req,res) => {
+    id = req.params.id;
+    res.send(`Id: ${id}`);
+    return;
+});
+
+app.get('/api/courses/:id/:name', (req,res) => {
+    res.send(req.params);
+    return;
+});
+
 app.listen(port, ()=>{
     console.log(`Listening on..${port}`);
 });
