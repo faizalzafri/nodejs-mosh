25716c1b42f81ee9a06f40d82762e2ffc2b53dd6
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-16 21:03:14 +0530

    input validation with Joi

--- express-app/index.js
+++ express-app/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 const app = express();
 
 app.use(bodyParser.json());
+
+const courseSchema = {
+    name: Joi.string().min(3).required()
+};
 
 const courses = [
     { id: 1, name: 'NodeJS' },
     { id: 2, name: 'Angular' }
 ];
- 
+
 app.get('/api/courses', (req, res) => {
     res.send(courses);
     res.end();
@@ -20,17 +25,27 @@ app.get('/api/courses/:id', (req, res) => {
     );
     if (!course) {
         res.status(404).send('Course with given id not found.')
-   }
+    }
     res.send(course);
     res.end();
 });
 
 app.post('/api/courses', (req, res) => {
-    if(req.body.name || req.body.name.length < 3){
-      res.status(400).send('Name is invalid');
-      res.end();
-      return;
+
+    const result = Joi.validate(req.body, courseSchema);
+
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        res.end();
+        return;
     }
+
+    // if(req.body.name || req.body.name.length < 3){
+    //   res.status(400).send('Name is invalid');
+    //   res.end();
+    //   return;
+    // }
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -43,3 +58,4 @@ app.post('/api/courses', (req, res) => {
 app.listen(3000, () => {
     console.log('Listening on..');
 });
+
