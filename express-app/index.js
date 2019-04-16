4e37407fef158b3c4749e362665c2e7bb5d80030
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-16 20:59:44 +0530

    input validation
    input validation for request body

--- express-app/index.js
+++ express-app/index.js
@@ -8,7 +8,7 @@ const courses = [
     { id: 1, name: 'NodeJS' },
     { id: 2, name: 'Angular' }
 ];
-
+ 
 app.get('/api/courses', (req, res) => {
     res.send(courses);
     res.end();
@@ -18,15 +18,19 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c =>
         c.id === parseInt(req.params.id)
     );
-
     if (!course) {
         res.status(404).send('Course with given id not found.')
-    }
+   }
     res.send(course);
     res.end();
 });
 
 app.post('/api/courses', (req, res) => {
+    if(req.body.name || req.body.name.length < 3){
+      res.status(400).send('Name is invalid');
+      res.end();
+      return;
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
