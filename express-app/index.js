5bf710a9ba84e823609ad0f9a8b4c0ca041ac772
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-16 20:55:03 +0530

    handling GET
    using body-parser

--- express-app/index.js
+++ express-app/index.js
@@ -1,23 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
 const courses = [
     { id: 1, name: 'NodeJS' },
     { id: 2, name: 'Angular' }
 ];
 
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+    res.end();
+});
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c =>
         c.id === parseInt(req.params.id)
     );
+
     if (!course) {
         res.status(404).send('Course with given id not found.')
     }
     res.send(course);
+    res.end();
 });
 
-app.listen(port, () => {
-    console.log(`Listening on..${port}`);
+app.listen(3000, () => {
+    console.log('Listening on..');
 });
