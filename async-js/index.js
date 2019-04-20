3ac739e660dfde713f6add6efa9fedd9b817360e
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-21 00:11:06 +0530

    dealing with async using nested callbacks

--- async-js/index.js
+++ async-js/index.js
@@ -1,12 +1,25 @@
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading User from database');
-        callback({ id: id, name: 'Zyx' });
+        const user = {
+            id: 1, name: 'Zyx'
+        }
+        callback(user);
+    }, 2000);
+}
+
+function getRepos(username, callback) {
+    setTimeout(() => {
+        console.log('Getting repos from github api');
+        callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }
 
 console.log('Before');
 const user = getUser(1, (user) => {
-    console.log('User: ' + user.id + ' ' + user.name);
+    console.log('User: ' + user.name);
+    getRepos(user.name, (repos) => {
+        console.log(repos);
+    })
 });
 console.log('After');
