62df99967dbd0cde2c9ffae4691c2f6a55638587
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-22 23:59:08 +0530

    parallel promises - all resolve

--- async-js/promise-api.js
+++ async-js/promise-api.js
@@ -1,6 +1,23 @@
-const p1 = Promise.resolve({id:1});
+const p1 = Promise.resolve({ id: 1 });
 p1.then(result => console.log(result));
 
 const p2 = Promise.reject(new Error('Error Occurred'));
 p2.catch(error => console.log(error.message));
+
+const parallelPromise1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async opn 1')
+        resolve(1);
+    }, 2000);
+});
+
+const parallelPromise2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async opn 2')
+        resolve(1);
+    }, 2000);
+});
+
+Promise.all([parallelPromise1, parallelPromise2])
+    .then(result => console.log(result));
 
