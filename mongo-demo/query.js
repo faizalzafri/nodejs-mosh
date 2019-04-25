1967ca7c6389a95e54fb27c958e034d2f40e957e
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-26 00:32:13 +0530

    simple query from db

---
+++ mongo-demo/query.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected'))
+    .catch(() => console.log('Failed to connect'));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getData() {
+    const courses = await Course.find();
+    console.log(courses);
+}
+
+getData();
