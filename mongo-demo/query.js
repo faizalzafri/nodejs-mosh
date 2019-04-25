d89dee383d66cc2a16d137da81b087d3d7acd2ec
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-26 00:36:58 +0530

    diff ways of customizing queries and filtering results

--- mongo-demo/query.js
+++ mongo-demo/query.js
@@ -15,8 +15,19 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema);
 
 async function getData() {
-    const courses = await Course.find();
+    const courses = await Course
+        .find({ isPublished: true })
+        .limit(10)
+        .sort('-name')
+        .select('name author');
     console.log(courses);
+
+    const courses2 = await Course
+        .find({ isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses2);
 }
 
 getData();
