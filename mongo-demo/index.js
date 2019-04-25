d63e8057e441108a60058b7ce428dbe11d2ee9eb
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-26 00:17:15 +0530

    creating schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,11 @@ mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected'))
     .catch(() => console.log('Failed to connect'));
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
