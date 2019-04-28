6b1ea827a38cb5c7bfe983b81d3208b3cea9ad69
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-04-28 21:00:58 +0530

    Code refactoring

--- mongo-demo/query.js
+++ mongo-demo/query.js
@@ -16,26 +16,15 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema);
 
 async function getData() {
-
+    const pageNumber = 2;
+    const pageSize = 10;
     const courses = await Course
         // .find({ author: /^Faizal/i })
         // .find({ author: /.*zal$/i })
         .find({ author: /.*Faiz.*/ })
-        .select();
-    console.log(courses);
-
-    const courses2 = await Course
-        .find()
-        .count();
-    console.log(courses2);
-
-    const pageNumber = 2;
-    const pageSize = 10;
-    const course3 = await Course
         .skip((pageNumber - 1) * pageSize)
         .limit(pageSize)
         .select({ name: 1 });
-
-    console.log(course3);
+    console.log(courses);
 }
 getData();
